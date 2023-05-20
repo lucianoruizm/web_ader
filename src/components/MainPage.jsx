1ca7bd1d32cd61c5832f06d3b433ac277159a43f
Luciano Ruiz <lucianoruizm993@gmail.com>
2023-05-20 13:31:37 -0300

    2

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,7 @@
 import { FaInstagram, FaFacebook } from "react-icons/fa";
 import styles from '../components/MainPage.module.css'
+import logoejemplo from '../assets/logoejemplo.png'
+import siluetabotella from '../assets/siluetabotella.png'
 
 export const MainPage = () => {
     return (
@@ -8,17 +10,28 @@ export const MainPage = () => {
                 <ul>
                     <li>Inicio</li>
                     <li>Productos</li>
+                    <li>Contacto</li>
                 </ul>
             </nav>
-            <div className={styles.productMediaContainer}>
-                <div className={styles.product}>
-                    <img className={styles.item1} src="../assets/siluetaBotella.png" alt="productoEjemplo" />
-                    <h2 className={styles.item2}>Producto Ejemplo</h2>
+            <div className={styles.productContainer}>
+                <img className={styles.productExample} src={siluetabotella} alt="productoEjemplo" />
+                <div className={styles.textContainer}>
+                    <h2 className={styles.productText}>Producto Ejemplo</h2>
                 </div>
-                <img className={styles.item3} src="../assets/logoEjemplo.png" alt="logo" />
+                <img className={styles.productExample} src={siluetabotella} alt="productoEjemplo" />
+                <div className={styles.textContainer}>
+                    <h2 className={styles.productText2}>Producto Ejemplo</h2>
+                </div>
+                <img className={styles.productExample} src={siluetabotella} alt="productoEjemplo" />
+                <div className={styles.textContainer}>
+                    <h2 className={styles.productText3}>Producto Ejemplo</h2>
+                </div>
+            </div>
+            <div className={styles.presentationContainer}>
+                <img className={styles.logo} src={logoejemplo} alt="logo" />
                 <div className={styles.socialMediaContainer}>
-                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram className={`${styles.socialMediaIcons}${styles.igIcon}`}/></a>
-                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FaFacebook className={`${styles.socialMediaIcons}${styles.fbIcon}`}/></a>
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram className={`${styles.socialMediaIcons} ${styles.igIcon}`}/></a>
+                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FaFacebook className={`${styles.socialMediaIcons} ${styles.fbIcon}`}/></a>
                 </div>
             </div>
         </div>
