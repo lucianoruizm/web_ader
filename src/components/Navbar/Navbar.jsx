27c922612e440b4c001718a6270e7d7bd45b98c5
Luciano Ruiz <lucianoruizm993@gmail.com>
2023-07-01 12:35:48 -0300

    responsive- icons navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,49 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css'
+import { AiOutlineHome } from "react-icons/ai"
+import { BsBag } from "react-icons/bs"
+import { BsBuildings } from "react-icons/bs"
+import { AiOutlineMail } from "react-icons/ai"
 
 export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-        <ul>
-            <Link to={'/'}>
-                <li>INICIO</li>
-            </Link>
-            <Link to={'/productos'}>
-                <li>PRODUCTOS</li>
-            </Link>
-            <Link to={'/empresa'}>
-                <li>EMPRESA</li>
-            </Link>
-            <Link to={'/contacto'}>
-                <li>CONTACTO</li>
-            </Link>
-        </ul>
+        <div className={styles.ulContainer}>
+            <ul>
+                <Link to={'/'}>
+                    <li>
+                        <div className={styles.homeContainer}>
+                            <AiOutlineHome />
+                            INICIO
+                        </div>
+                    </li>
+                </Link>
+                <Link to={'/productos'}>
+                    <li>
+                        <div className={styles.homeContainer}>
+                            <BsBag />
+                            PRODUCTOS
+                        </div>
+                    </li>
+                </Link>
+                <Link to={'/empresa'}>
+                    <li>
+                        <div className={styles.homeContainer}>
+                            <BsBuildings />
+                            EMPRESA
+                        </div>
+                    </li>
+                </Link>
+                <Link to={'/contacto'}>
+                    <li>
+                        <div className={styles.homeContainer}>
+                            <AiOutlineMail />
+                            CONTACTO
+                        </div>
+                    </li>
+                </Link>
+            </ul>
+        </div>
     </nav>
   )
 }
